333afceedeee12143f1dbcd46c56093c7b0a6dbb
unknown <steen_hansen@yahoo.com>
2022-06-02 13:06:12 -0700

    link to git hub

--- pages/index/index.page.jsx
+++ pages/index/index.page.jsx
@@ -14,6 +14,12 @@ function Page() {
           Interactive. <Counter init_count={"1"} />
         </li>
       </ul>
+      <br />
+      <br />
+       <a href="https://github.com/steenhansen/type-czech-canonical">
+        https://github.com/steenhansen/type-czech-canonical
+      </a>
+
     </>
   );
 }
